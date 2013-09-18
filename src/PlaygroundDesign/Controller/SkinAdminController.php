ef15cc6209789d0fdd77d390b07bccc3704a8527
Thomas ROGER <thomas.roger@adfab.fr>
2013-09-18 18:07:49 +0200

    PLAYGROUND-142, add company & skin controller

---
+++ src/PlaygroundDesign/Controller/SkinAdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PlaygroundDesign\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class SkinAdminController extends AbstractActionController
+{
+    public function listAction()
+    {
+      return array();
+    }
+}
