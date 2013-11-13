ab321f9477aad7352783dabed1e8496e3fae7fff
Thomas ROGER <thomas.roger@adfab.fr>
2013-11-13 14:20:21 +0100

    generate dynamiq conf for ckeditor, myop-20

---
+++ view/admin/assets/js/ckeditor-custom/ckeditor-styles.js
@@ -0,0 +1,17 @@
+CKEDITOR.stylesSet.add( 'custom_styles',
+[
+
+	/* Block Styles */
+	
+	{ name: 'Slider', 				element: 'div', 	attributes: { 'class': 'default-slider' } },
+	{ name: 'Citation',				element: 'p',		attributes: { 'class': 'citation' } },
+
+    { name: 'CitationThomas',             element: 'p',       attributes: { 'class': 'citationTHomas' } },
+	
+	
+	/* Inline Styles */
+	{ name: 'Chapô',				element: 'samp' },
+    
+    /* Object Styles */
+    
+]);
