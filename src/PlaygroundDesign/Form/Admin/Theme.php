91432fe7b3d13e99e4e7564ebed2a44f74fe3c6e
Thomas ROGER <thomas.roger@adfab.fr>
2014-07-04 16:01:57 +0200

    maj 2.2.6 to 2.3.*, PLAYGROUND-502

--- src/PlaygroundDesign/Form/Admin/Theme.php
+++ src/PlaygroundDesign/Form/Admin/Theme.php
@@ -4,7 +4,7 @@ namespace PlaygroundDesign\Form\Admin;
 
 use Zend\Form\Element;
 use ZfcBase\Form\ProvidesEventsForm;
-use Zend\I18n\Translator\Translator;
+use Zend\Mvc\I18n\Translator;
 use Zend\ServiceManager\ServiceManager;
 
 class Theme extends ProvidesEventsForm
@@ -19,7 +19,7 @@ class Theme extends ProvidesEventsForm
     *
     * @param string $name
     * @param Zend\ServiceManager\ServiceManager $serviceManager 
-    * @param Zend\I18n\Translator\Translator $translator
+    * @param Zend\Mvc\I18n\Translator $translator
     *
     */
     public function __construct($name = null, ServiceManager $serviceManager, Translator $translator)
