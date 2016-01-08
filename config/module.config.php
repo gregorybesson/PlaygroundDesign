030b0a6d8735f0d0f9383f9a1bdc51970cfac827
gregorybesson <gregory.besson@adfab.fr>
2016-01-08 12:36:01 +0100

    Translation file is searched in base directory 'language'

--- config/module.config.php
+++ config/module.config.php
@@ -624,7 +624,7 @@ return array(
         'translation_file_patterns' => array(
             array(
                 'type' => 'phpArray',
-                'base_dir' => __DIR__ . '/../../../../../language',
+                'base_dir' => __DIR__ . '/../../../../language',
                 'pattern' => '%s.php',
                 'text_domain' => 'playgrounddesign'
             ),
