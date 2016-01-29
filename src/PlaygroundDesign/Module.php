0d410857b8fd13e57d9dcb1b271fbef7fe3c7ac9
gregorybesson <gregory.besson@adfab.fr>
2016-01-29 07:55:36 +0100

    headtitle improvement

--- src/PlaygroundDesign/Module.php
+++ src/PlaygroundDesign/Module.php
@@ -470,23 +470,17 @@ class Module implements
             $area = isset($match)? $match->getParam('area', ''):'';
             $sm = $e->getApplication()->getServiceManager();
             if($match){
+                $title = $match->getParam('title');
                 $action = $match->getParam('action');
                 $controller = explode('\\', $match->getParam('controller'));
                 $controller = end($controller);
-                $module = __NAMESPACE__;
                 $headTitleHelper = $sm->get('viewHelperManager')->get('headTitle');
-                $title = $module . '-' . $controller . '-' . $action;
+
+                if(empty($title)) $title = $controller . '-' . $action;
                 $title = $sm->get('translator')->translate($title, 'routes');
 
                 if($title !== ' ' && !empty($title)){
-                    if(!empty($headTitleHelper->renderTitle())){
-                        $title = $sm->get('translator')->translate(
-                            $headTitleHelper->renderTitle() . ' - ' . $title,
-                            'routes'
-                        );
-                    }
-
-                    $headTitleHelper->set($title);
+                    $headTitleHelper->prepend($title);
                 }
             }
 
@@ -495,7 +489,7 @@ class Module implements
             foreach ($viewModel->getChildren() as $child) {
                 $child->area = $area;
             }
-        });
+        },-1000);
     }
 
     public function getAutoloaderConfig()
